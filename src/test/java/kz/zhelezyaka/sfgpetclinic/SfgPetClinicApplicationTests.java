e04ee016c2dbe1980eba24ea2aab307fc638f570
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-09-08 06:50:00 +0600

    Initial Project Add

---
+++ src/test/java/kz/zhelezyaka/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
