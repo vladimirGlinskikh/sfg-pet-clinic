915d615ffa1c4d47195e603cd1672c1b2dc28626
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-11-20 17:01:40 +0600

    Adding Spring config. Closes #22

--- pet-clinic-data/src/main/java/kz/zhelezyaka/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/kz/zhelezyaka/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -2,9 +2,11 @@ package kz.zhelezyaka.sfgpetclinic.services.map;
 
 import kz.zhelezyaka.sfgpetclinic.model.Owner;
 import kz.zhelezyaka.sfgpetclinic.services.OwnerService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService{
     @Override
     public Set<Owner> findAll() {
