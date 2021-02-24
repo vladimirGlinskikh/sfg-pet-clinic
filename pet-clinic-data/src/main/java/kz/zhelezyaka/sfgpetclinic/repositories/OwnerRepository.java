9c008c2eb0c6ff9c4a5977da9d242291c2672214
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-02-24 19:56:08 +0600

    Added spring data jpa service for owners. Closes #40

--- pet-clinic-data/src/main/java/kz/zhelezyaka/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/kz/zhelezyaka/sfgpetclinic/repositories/OwnerRepository.java
@@ -4,5 +4,5 @@ import kz.zhelezyaka.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
-
+    Owner findByLastName(String lastName);
 }
