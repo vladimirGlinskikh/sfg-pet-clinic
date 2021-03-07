c46484507f68c845d9d4d5f9cd41d7d075af094a
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-03-07 23:31:11 +0600

    Upgraded to JUnit 5. Closes #4

---
+++ pet-clinic-web/src/test/java/kz/zhelezyaka/sfgpetclinic/SfgPetClinicApplicationTest.java
@@ -0,0 +1,15 @@
+package kz.zhelezyaka.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class SfgPetClinicApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
