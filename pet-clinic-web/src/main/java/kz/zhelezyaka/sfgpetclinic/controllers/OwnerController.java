1099a46338aa85889f8dfb24bd5b0ba5938b18e0
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-04-10 16:09:32 +0600

    Spring Pet Clinic - Web Data Binder

--- pet-clinic-web/src/main/java/kz/zhelezyaka/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/kz/zhelezyaka/sfgpetclinic/controllers/OwnerController.java
@@ -3,7 +3,9 @@ package kz.zhelezyaka.sfgpetclinic.controllers;
 import kz.zhelezyaka.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,6 +17,11 @@ public class OwnerController {
 
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
     }
 
     @RequestMapping({"", "/", "/index", "/index.html"})
@@ -29,7 +36,7 @@ public class OwnerController {
     }
 
     @GetMapping("/{ownerId}")
-    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId){
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
         ModelAndView mav = new ModelAndView("owners/ownerDetails");
         mav.addObject(ownerService.findById(ownerId));
         return mav;
