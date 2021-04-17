673abcea0f11ee95676ed11626eb87bd3af3c5ca
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-04-17 19:34:04 +0600

    Added add pet, update pet

--- pet-clinic-data/src/main/java/kz/zhelezyaka/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/kz/zhelezyaka/sfgpetclinic/model/PetType.java
@@ -10,7 +10,6 @@ import javax.persistence.Table;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
--- pet-clinic-web/src/test/java/kz/zhelezyaka/sfgpetclinic/SfgPetClinicApplicationTest.java
+++ pet-clinic-web/src/test/java/kz/zhelezyaka/sfgpetclinic/SfgPetClinicApplicationTest.java
@@ -5,11 +5,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SfgPetClinicApplicationTest {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 }
